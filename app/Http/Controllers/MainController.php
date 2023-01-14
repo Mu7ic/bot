94cd763c6e876128733308e807c49177e89d7d91
Mu7ic <m928344074@gmail.com>
2023-01-14 15:44:20 +0300

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+
+class MainController extends Controller
+{
+    public $domainTelegram;
+    public $token;
+    public $host;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->domainTelegram = env('TELEGRAM_API').env('TOKEN').'/';
+        $this->token = env('TOKEN');
+        $this->host = env('HOST');
+    }
+
+    public function setWebhook()
+    {
+        $client = new Client(['base_uri' => $this->domainTelegram]);
+        $response = $client->get("setWebhook?url={$this->host}");
+        return response()->json($response->getBody());
+    }
+}
